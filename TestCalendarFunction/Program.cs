1b42ac675862926401e24d4d6d274ed9f1e6ae7a
@Pikhota <s.pikhota@gmail.com>
2017-06-13 11:19:27 +0300

    refactoring

--- TestCalendarFunction/Program.cs
+++ TestCalendarFunction/Program.cs
@@ -11,7 +11,7 @@ namespace TestCalendarFunction
             var startDate = Function.GetDate();
             var duration = Function.Duration;
             var weekends = new List<DateTime>();
-            weekends.AddRange(Function.Weekends(new DateTime(2017, 4, 23), new DateTime(2017, 4, 25)));
+            weekends.AddRange(Function.Weekends(new DateTime(2017, 4, 23), new DateTime(2017, 4, 21)));
             
             Console.WriteLine($"Finish date is:{Function.GetFinalDate(startDate, duration, weekends)}");
             Console.ReadLine();
